bc3f9e0924df4433903fde28e6ee2b40aa455e88
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-10-02 22:58:20 -0700

    added gallery section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,9 +30,15 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        {/* Logo */}
-        <div className="logo">
-          <img src="/logo.svg" alt="Granville Tattoo" className="logo-img" />
+        {/* Contact Info */}
+        <div className="contact-info">
+          <a href="tel:6046859800" className="phone-link">
+            <svg className="phone-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+            </svg>
+          </a>
+          <div className="divider"></div>
+          <div className="address">1007 Granville St Vancouver</div>
         </div>
 
         {/* Desktop Navigation */}
