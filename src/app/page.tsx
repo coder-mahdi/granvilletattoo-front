1c17aa6091785f05294fb5f4cd627fe9e961e79a
mahdi-mim <mahdiroozbahani25@gmail.com>
2026-04-10 17:52:42 -0700

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-
-'use client';
-
-import { useState, useEffect } from 'react';
-import LoadingAnimation from '@/components/animations/LoadingAnimation';
 import HeroSection from '@/modules/home/HeroSection';
 import AboutSection from '@/modules/home/AboutSection';
 import ArtistSection from '@/modules/home/ArtistSection';
@@ -10,27 +5,9 @@ import GallerySection from '@/modules/home/GallerySection';
 import TestimonialsSection from '@/components/TestimonialsSection';
 
 export default function Home() {
-  const [showContent, setShowContent] = useState(false);
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    let loaderTimeout: ReturnType<typeof setTimeout> | undefined;
-    const timer = setTimeout(() => {
-      setShowContent(true);
-      loaderTimeout = setTimeout(() => setShowLoader(false), 500);
-    }, 4000);
-
-    return () => {
-      clearTimeout(timer);
-      if (loaderTimeout) clearTimeout(loaderTimeout);
-    };
-  }, []);
-
   return (
     <section className="home">
-      {showLoader && <LoadingAnimation showContent={showContent} />}
-      
-      <div className={`main-content ${showContent ? 'show' : ''}`}>
+      <div className="main-content">
         <HeroSection />
         <GallerySection />
         <ArtistSection />
